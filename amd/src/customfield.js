fb411028e8e40d2919099882639ae87b171c4720
Thomas Winkler <winkler_t@gmx.at>
2021-12-09 14:21:57 +0100

    1st js draft

---
+++ amd/src/customfield.js
@@ -0,0 +1,84 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@winnidat 
+Wunderbyte-GmbH
+/
+moodle-local_wunderbyte_table
+Public
+2
+0
+0
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+moodle-local_wunderbyte_table/amd/src/init.js /
+
+georgmaisser Include js call inside (to make it work also with Ajax)
+Latest commit 7869ef3 on 26 Oct
+ History
+ 1 contributor
+262 lines (237 sloc)  8.71 KB
+   
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * @package    local_wunderbyte_table
+ * @copyright  Wunderbyte GmbH <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Gets called from mustache template.
+ * @param {string} catid
+ * @param {string} encodedtable
+ */
+export const init = () => {
+    reloadCats();
+    addEvents();
+};
+
+function reloadCats() {
+    catfieldsets = document.querySelectorAll('[id^=id_category]');
+    select = document.getElementById('id_type').value;
+    for (var i = 0, len = catfieldsets.length + 0; i < len; i++) {
+        if (i + 1 == select) { 
+            catfieldsets[i].hidden = false;
+            catfieldsets[i].disabled = false;
+        } else {         
+        catfieldsets[i].hidden = true;
+        catfieldsets[i].disabled = true;
+        }
+    }
+}
+
+function addEvents() {
+    select = document.getElementById('id_type');
+    select.addEventListener('change', () => {
+        loadCategories();
+    });
+}
+
