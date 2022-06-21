b4d62b69d6cd2a0703d5a288c67b1073cf12712e
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2022-06-21 15:21:11 +0200

    Version 0.0.42 (2022062100)

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_entities';
-$plugin->release = '0.0.41';
-$plugin->version = 2022060101;
+$plugin->release = '0.0.42';
+$plugin->version = 2022062100;
 $plugin->requires = 2020061500;
 $plugin->supported = [39, 311];
 $plugin->maturity = MATURITY_ALPHA;
