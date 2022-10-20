27fdf7b65575c84342776c87f6843d5b5bb766b8
georgmaisser <georg.maisser@wunderbyte.at>
2022-10-20 17:15:31 +0200

    add service provider interface & entitydate entity

---
+++ classes/local/callback/service_provider.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the local_entities\local\callback\service_provider interface.
+ *
+ * Plugins should implement this if they use entities subsystem.
+ *
+ * @package local_entities
+ * @copyright 2022 Georg Maißer Wunderbyte GmbH <info@wunderbyte.at>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_entities\local\callback;
+
+/**
+ * The service_provider interface for plugins to provide callbacks which are needed by the entities subsystem.
+ *
+ * @copyright  2022 Georg Maißer Wunderbyte GmbH <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+interface service_provider {
+
+    /**
+     * This function takes a list of ids and returns a list of dates in the entitydate format.
+     * As one plugin can use multiple different handlers...
+     * ... we use $area to specify the handler of the component.
+     *
+     * @param array $ids
+     * @param string $area
+     * @return array
+     */
+    public static function return_array_of_dates(array $ids, string $area):array;
+}
---
+++ classes/local/entities/entitydate.php
@@ -0,0 +1,127 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The entitydate class.
+ *
+ * @package    local_entities
+ * @copyright  2022 Georg Maißer Wunderbyte GmbH <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_entities\local\entities;
+
+/**
+ * The entitydate class.
+ *
+ * @copyright  2022 Georg Maißer Wunderbyte GmbH <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class entitydate {
+
+    /**
+     * Item id
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * Component
+     *
+     * @var string
+     */
+    private $component;
+
+    /**
+     * Area
+     *
+     * @var string
+     */
+    private $area;
+
+    /**
+     * Name
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * Starttime as timestamp
+     *
+     * @var int
+     */
+    private $starttime;
+
+    /**
+     * Endtime as timestamp
+     *
+     * @var int
+     */
+    private $endtime;
+
+    /**
+     * Status
+     *
+     * @var int
+     */
+    private $status;
+
+    /**
+     * Cunstructor.
+     *
+     * @param int $itemid
+     * @param string $component
+     * @param string $area
+     * @param string $name
+     * @param int $starttime
+     * @param int $endtime
+     * @param int $status
+     */
+    public function __construct(int $itemid,
+                                string $component,
+                                string $area,
+                                string $name,
+                                int $starttime,
+                                int $endtime,
+                                int $status) {
+        $this->itemid = $itemid;
+        $this->component = $component;
+        $this->area = $area;
+        $this->name = $name;
+        $this->starttime = $starttime;
+        $this->endtime = $endtime;
+        $this->status = $status;
+    }
+
+    /**
+     * Returns all the values as array.
+     *
+     * @return array
+     */
+    public function getitem():array {
+        $item = array();
+        $item['itemid'] = $this->itemid;
+        $item['component'] = $this->component;
+        $item['area'] = $this->area;
+        $item['name'] = $this->name;
+        $item['starttime'] = $this->starttime;
+        $item['endtime'] = $this->endtime;
+        $item['status'] = $this->status;
+        return $item;
+    }
+}
