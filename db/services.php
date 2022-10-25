5f699b59dd3211027a08b6ae0cca032dc7b11175
Thomas Winkler <thomas.winkler@wunderbyte.at>
2022-10-25 11:27:44 +0200

    linting

--- db/services.php
+++ db/services.php
@@ -69,6 +69,7 @@ $functions = array(
         'local_entities_get_entity_calendardata' => array(
                 'classname' => 'local_entities\external\get_entity_calendardata',
                 'classpath' => '',
+                'methodname' => 'get_entity_calendardata',
                 'description' => 'Get calendardata from specific entity',
                 'type' => 'read',
                 'ajax' => true
