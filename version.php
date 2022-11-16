4cdfe07a12037304eb81c0316a50f8efeb7ba2cc
georgmaisser <georg.maisser@wunderbyte.at>
2022-11-16 21:54:31 +0100

    Version 0.1.1 (2022111600)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_entities';
-$plugin->release = '0.1.0';
-$plugin->version = 2022103004;
+$plugin->release = '0.1.1';
+$plugin->version = 2022111600;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
