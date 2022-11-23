3d886178e4e86efd5f0e12fe181842d1200e31d7
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2022-11-23 12:41:36 +0100

    Version 0.1.2 (2022112300)

---
+++ CHANGES.md
@@ -0,0 +1,3 @@
+## Version 0.1.2 (2022112300)
+**Improvements:**
+* Small design improvements and linting.
--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_entities';
-$plugin->release = '0.1.1';
-$plugin->version = 2022112103;
+$plugin->release = '0.1.2';
+$plugin->version = 2022112300;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
