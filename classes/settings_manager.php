72a62274f8e975a127ba00ca0b4d32fe25db3ba3
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2024-06-24 18:04:00 +0200

    Codestyle

--- classes/settings_manager.php
+++ classes/settings_manager.php
@@ -44,7 +44,7 @@ class settings_manager {
 
     /**
      * Entity constructor.
-     * @param int $id
+     * @param ?int $id
      */
     public function __construct(?int $id = null) {
         $this->id = $id;
