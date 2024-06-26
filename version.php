fabb8c33809a10f570b6e014723fa535c52b9001
Georg Maißer <georg.maisser@wunderbyte.at>
2024-06-26 12:02:21 +0200

    Version (2024062600)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_entities';
-$plugin->release = '0.3.6';
-$plugin->version = 2024062501;
+$plugin->release = '0.3.7';
+$plugin->version = 2024062600;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
